447575c287621c9bd5fba53da1d254b56f4a5928
Beleda, Praveen (C) (GB) <Praveen.Beleda@cubic.com>
2017-06-08 21:37:18 +0100

    Added file

---
+++ src/main/java/com/cubic/accelerators/WebDriverConstants.java
@@ -0,0 +1,10 @@
+package com.cubic.accelerators;
+
+public interface WebDriverConstants {
+	String FIREFOX_BROWSER = "firefox";
+	String CHROME_BROWSER = "chrome";
+	String IE_BROWSER = "ie";
+	String EDGE_BROWSER = "edge";
+	String SAFARI_BROWSER = "Safari";
+	String LOCAL = "local";	
+}
